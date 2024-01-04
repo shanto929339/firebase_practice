df77664bf334f0e439c779d06c51fa5dfd5d2604
Shanto <shantokhan929339@gmail.com>
2024-01-04 18:03:57 +0600

    creatUser infot

--- lib/View/Screnns/UserDetailsScreen/user_details_screen.dart
+++ lib/View/Screnns/UserDetailsScreen/user_details_screen.dart
@@ -18,75 +18,136 @@ class _UserDetailsState extends State<UserDetails> {
   TextEditingController nameController=TextEditingController();
   TextEditingController designationController=TextEditingController();
   TextEditingController locationController=TextEditingController();
-
+  TextEditingController prNameController=TextEditingController();
+  TextEditingController productcetagoryController=TextEditingController();
+  TextEditingController productPriceController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 24,horizontal: 20),
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10.0,),
+              TextField(
+                controller:nameController,
+                decoration: const InputDecoration(
+                    hintText:"Name",
+                    border:OutlineInputBorder(
+                      borderRadius:  BorderRadius.all(Radius.circular(30.0)),
 
-          const SizedBox(height: 10.0,),
-          TextField(
-            controller:nameController,
-            decoration: const InputDecoration(
-                hintText:"Name",
-                border:OutlineInputBorder(
-                  borderRadius:  BorderRadius.all(Radius.circular(30.0)),
+                    )
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+              TextField(
+                controller:designationController,
+                decoration: const InputDecoration(
+                    hintText:"Designation",
+                    border:OutlineInputBorder(
+                      borderRadius:  BorderRadius.all(Radius.circular(30.0)),
 
-                )
-            ),
+                    )
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+
+              TextField(
+                controller:locationController,
+                decoration: const InputDecoration(
+                    hintText:"Location",
+                    border:OutlineInputBorder(
+                      borderRadius:  BorderRadius.all(Radius.circular(30.0)),
+                    )
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+
+              ElevatedButton(onPressed:(){creatUserdetaila();},child:Text("Submit "),),
+
+              SizedBox(height:30.0),
+
+
+
+              const SizedBox(height: 10.0,),
+              TextField(
+                controller:prNameController,
+                decoration: const InputDecoration(
+                    hintText:"Product_name",
+                    border:OutlineInputBorder(
+                      borderRadius:  BorderRadius.all(Radius.circular(30.0)),
+
+                    )
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+              TextField(
+                controller:productcetagoryController,
+                decoration: const InputDecoration(
+                    hintText:"Cetagory",
+                    border:OutlineInputBorder(
+                      borderRadius:  BorderRadius.all(Radius.circular(30.0)),
+
+                    )
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+
+              TextField(
+                controller:productPriceController,
+                decoration: const InputDecoration(
+                    hintText:"Price",
+                    border:OutlineInputBorder(
+                      borderRadius:  BorderRadius.all(Radius.circular(30.0)),
+                    )
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+
+
+
+              ElevatedButton(onPressed:(){creatProduct();},child:Text("Submit "),),
+
+
+
+
+            ],
           ),
-          const SizedBox(height: 10.0,),
-          TextField(
-            controller:designationController,
-            decoration: const InputDecoration(
-                hintText:"Designation",
-                border:OutlineInputBorder(
-                  borderRadius:  BorderRadius.all(Radius.circular(30.0)),
+        ),
 
-                )
-            ),
-          ),
-          const SizedBox(height: 10.0,),
-
-          TextField(
-            controller:locationController,
-            decoration: const InputDecoration(
-                hintText:"Location",
-                border:OutlineInputBorder(
-                  borderRadius:  BorderRadius.all(Radius.circular(30.0)),
-                )
-            ),
-          ),
-          const SizedBox(height: 10.0,),
-
-
-
-          ElevatedButton(onPressed:(){creatUserdetaila();},child:Text("Submit "),),
-
-
-        ],
       ),
-
     );
   }
 
-
   creatUserdetaila()async{
-
-     CollectionReference _collectionRef= FirebaseFirestore.instance.collection("user_details");
-
-     return _collectionRef.doc(currentUser()).set({
-       "Name":nameController.text.toString().trim(),
-       "Designation":designationController.text.toString().trim(),
-        "Location":locationController.text.toString().trim(),
+         CollectionReference _collectionRef= FirebaseFirestore.instance.collection("user_details");
+        return _collectionRef.doc(currentUser()).set({
+           "Name":nameController.text.toString().trim(),
+           "Designation":designationController.text.toString().trim(),
+           "Location":locationController.text.toString().trim(),
 
      }).then((value) =>Get.snackbar("","added Successfully"),);
+  }
+
+  creatProduct(){
+    CollectionReference productCollection=FirebaseFirestore.instance.collection("product_list");
+
+    return productCollection.doc().set({
+
+      "Product_name":prNameController.text.toString().trim(),
+      "Product_cetagory":productcetagoryController.text.toString().trim(),
+      "product_price":productPriceController.text.toString().trim(),
+
+    }).then((value) => print("error"),);
+
 
 
   }
+
 
 }
